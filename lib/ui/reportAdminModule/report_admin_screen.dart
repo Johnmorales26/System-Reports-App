a7b4183b3db839701a26ef161c32c3f38eb7d3d6
Jonatan Morales <johnta2610@hotmail.com>
2024-10-13 11:46:23 -0600

    🌟 Feat: Add user selection via AlertDialog

--- lib/ui/reportAdminModule/report_admin_screen.dart
+++ lib/ui/reportAdminModule/report_admin_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:system_reports_app/data/models/data_entry.dart';
+import 'package:system_reports_app/data/local/user_database.dart';
 import 'package:system_reports_app/ui/reportAdminModule/report_admin_view_model.dart';
 
 class ReportAdminScreen extends StatelessWidget {
@@ -21,121 +21,165 @@ class _DataEntryFormState extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final TextEditingController referenceNumberController =
-        TextEditingController();
-    final TextEditingController clientController = TextEditingController();
-    final TextEditingController locationController = TextEditingController();
-    final TextEditingController fseNameController = TextEditingController();
-    final TextEditingController customManagerController =
-        TextEditingController();
-    final TextEditingController activityPerformedController =
-        TextEditingController();
-    final TextEditingController observationsController =
-        TextEditingController();
-
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Data Entry Form'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              controller: referenceNumberController,
-              decoration: const InputDecoration(
-                labelText: 'Número de referencia',
-                border: OutlineInputBorder(), // Establece el borde a Outlined
-              ),
-            ),
-            const SizedBox(height: 10), // Espacio entre los campos
-            TextField(
-              controller: clientController,
-              decoration: const InputDecoration(
-                labelText: 'Cliente',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              controller: locationController,
-              decoration: const InputDecoration(
-                labelText: 'Locación',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              controller: fseNameController,
-              decoration: const InputDecoration(
-                labelText: 'Nombre FSE',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              controller: customManagerController,
-              decoration: const InputDecoration(
-                labelText: 'Custom Manager',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              controller: activityPerformedController,
-              decoration: const InputDecoration(
-                labelText: 'Activity Performed',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              controller: observationsController,
-              decoration: const InputDecoration(
-                labelText: 'Observaciones',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                // Aquí puedes manejar el envío de datos
-                final dataEntry = DataEntry(
-                  referenceNumber: referenceNumberController.text,
-                  client: clientController.text,
-                  location: locationController.text,
-                  fseName: fseNameController.text,
-                  customManager: customManagerController.text,
-                  activityPerformed: activityPerformedController.text,
-                  observations: observationsController.text,
-                );
-                final response = await vm.uploadData(dataEntry);
-                if (response) {
-                  referenceNumberController.clear();
-                  clientController.clear();
-                  locationController.clear();
-                  fseNameController.clear();
-                  customManagerController.clear();
-                  activityPerformedController.clear();
-                  observationsController.clear();
+        appBar: AppBar(
+          title: const Text('Data Entry Form'),
+        ),
+        body: FutureBuilder(
+            future: vm.fetchAllUsers(),
+            builder: (BuildContext context,
+                AsyncSnapshot<List<UserDatabase>> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Container();
+              } else if (snapshot.hasData) {
+                final users = snapshot.data!;
+                if (users.isNotEmpty) {
+                  return Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        TextField(
+                          controller: vm.referenceNumberController,
+                          decoration: const InputDecoration(
+                            labelText: 'Número de referencia',
+                            border:
+                                OutlineInputBorder(), // Establece el borde a Outlined
+                          ),
+                        ),
+                        const SizedBox(height: 10), // Espacio entre los campos
+                        TextField(
+                          controller: vm.clientController,
+                          decoration: const InputDecoration(
+                            labelText: 'Cliente',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        TextField(
+                          controller: vm.locationController,
+                          decoration: const InputDecoration(
+                            labelText: 'Locación',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        GestureDetector(
+                          onTap: () {
+                            _showAlertDialog(context, users, vm);
+                          },
+                          child: AbsorbPointer(
+                              child: TextField(
+                            controller: vm.fseNameController,
+                            decoration: const InputDecoration(
+                              labelText: 'Nombre FSE',
+                              border: OutlineInputBorder(),
+                            ),
+                          )),
+                        ),
+                        const SizedBox(height: 10),
+                        TextField(
+                          controller: vm.customManagerController,
+                          decoration: const InputDecoration(
+                            labelText: 'Custom Manager',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        TextField(
+                          controller: vm.activityPerformedController,
+                          decoration: const InputDecoration(
+                            labelText: 'Activity Performed',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        const SizedBox(height: 10),
+                        TextField(
+                          controller: vm.observationsController,
+                          decoration: const InputDecoration(
+                            labelText: 'Observaciones',
+                            border: OutlineInputBorder(),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        ElevatedButton(
+                          onPressed: () async {
+                            final response = await vm.uploadData();
+                            if (response) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text('Datos enviados con éxito')),
+                              );
 
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Datos enviados con éxito')),
+                              Navigator.pop(context);
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text('Error al enviar los datos')),
+                              );
+                            }
+                          },
+                          child: const Text('Enviar'),
+                        ),
+                      ],
+                    ),
                   );
-
-                  Navigator.pop(context);
                 } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Error al enviar los datos')),
-                  );
+                  return Container();
                 }
+              } else {
+                return Container();
+              }
+            }));
+  }
+
+  void _showAlertDialog(BuildContext context, List<UserDatabase> users, ReportAdminViewModel vm) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Selecciona un usuario'),
+          content: SizedBox(
+            width: double
+                .maxFinite, // Asegura que el contenido use el máximo ancho
+            height:
+                300, // Establece una altura fija para que no se extienda demasiado
+            child: ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (context, index) {
+                final currentUser = users[index];
+                return ListTile(
+                  title: Text(currentUser.name),
+                  subtitle: Text(currentUser.fseName),
+                  leading: currentUser.image.isNotEmpty
+                      ? ClipOval(
+                          child: Image.network(
+                          currentUser.image,
+                          width: 50.0,
+                          height: 50.0,
+                          fit: BoxFit.cover,
+                        ))
+                      : Container(),
+                  onTap: () {
+                    vm.updateFSEController(currentUser.fseName);
+                    Navigator.of(context).pop();
+                  },
+                );
               },
-              child: const Text('Enviar'),
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Close'),
             ),
           ],
-        ),
-      ),
+        );
+      },
     );
   }
 }
--- lib/ui/reportAdminModule/report_admin_view_model.dart
+++ lib/ui/reportAdminModule/report_admin_view_model.dart
@@ -1,16 +1,67 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:system_reports_app/data/local/user_database.dart';
 import 'package:system_reports_app/data/models/data_entry.dart';
 import 'package:system_reports_app/utils/constants.dart';
 
 class ReportAdminViewModel extends ChangeNotifier {
-  Future<bool> uploadData(DataEntry dataEntry) async {
+  final TextEditingController referenceNumberController =
+      TextEditingController();
+  final TextEditingController clientController = TextEditingController();
+  final TextEditingController locationController = TextEditingController();
+  final TextEditingController customManagerController = TextEditingController();
+  final TextEditingController activityPerformedController =
+      TextEditingController();
+  final TextEditingController observationsController = TextEditingController();
+  final TextEditingController fseNameController = TextEditingController();
+
+  Future<bool> uploadData() async {
+    final dataEntry = DataEntry(
+      referenceNumber: referenceNumberController.text,
+      client: clientController.text,
+      location: locationController.text,
+      fseName: fseNameController.text,
+      customManager: customManagerController.text,
+      activityPerformed: activityPerformedController.text,
+      observations: observationsController.text,
+    );
     final FirebaseFirestore _firestore = FirebaseFirestore.instance;
     try {
-      await _firestore.collection(Constants.COLLECTION_PENDING_TASK).add(dataEntry.toJson());
+      await _firestore
+          .collection(Constants.COLLECTION_PENDING_TASK)
+          .add(dataEntry.toJson());
+      _clearForm();
       return true;
     } catch (e) {
       return false;
     }
+  }
+
+  Future<List<UserDatabase>> fetchAllUsers() async {
+    try {
+      CollectionReference collectionRef =
+          FirebaseFirestore.instance.collection(Constants.COLLECTION_USERS);
+      QuerySnapshot querySnapshot = await collectionRef.get();
+      List<UserDatabase> users = querySnapshot.docs.map((doc) {
+        return UserDatabase.fromJson(doc.data() as Map<String, dynamic>);
+      }).toList();
+      return users;
+    } catch (e) {
+      return [];
+    }
+  }
+
+  void updateFSEController(String fseName) {
+    fseNameController.text = fseName;
+  }
+
+  void _clearForm() {
+    referenceNumberController.clear();
+    clientController.clear();
+    locationController.clear();
+    fseNameController.clear();
+    customManagerController.clear();
+    activityPerformedController.clear();
+    observationsController.clear();
   }
 }
